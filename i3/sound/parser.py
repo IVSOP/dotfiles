d088f4b90ba8ca3573adc2f5e65eb8b6d90d294a
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2024-12-18 16:20:34 +0000

    remembered I know regex

--- i3/sound/parser.py
+++ i3/sound/parser.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
-txt = (input().split("/"))[1]
-txt = txt[2:len(txt)-2]
-# for some reason at 0% it gets an extra space (" 0%" != "0%")
-if txt[0] == " ":
-    print(txt[1:])
+import re
+
+match = re.search(r'(\d+)%', input())
+if match:
+    print(match.group(1))
 else:
-    print(txt)
+    print("0")
+
+
+# txt = (input().split("/"))[1]
+# txt = txt[2:len(txt)-2]
+# # for some reason at 0% it gets an extra space (" 0%" != "0%")
+# if txt[0] == " ":
+#     print(txt[1:])
+# else:
+#     print(txt)
