b26f174e2cb56411a92ae7b2d185a4211c7ee1a0
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2024-12-28 00:06:42 +0000

    harpoon keybinds

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -14,6 +14,8 @@ return {
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end) -- I use telescope, not this
+        vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
+        vim.keymap.set("n", "<leader>x", function() harpoon:list():clear() end)
 
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
