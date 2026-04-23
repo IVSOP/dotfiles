795ff0d78fd50a90eeb7436df19d2501918afb53
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2026-04-23 13:38:45 +0100

    lock

--- sway/lock/lock.sh
+++ sway/lock/lock.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-
-# FIXME: need to make this work with multiple monitors
+# Works with multiple monitors by handling each output separately
 
 NOISE_PATH=$($HOME/Desktop/Rofi-Themer/Scripts/read.sh "i3lock")
-
-if [[ -z $NOISE_PATH ]]
-then
-    return
+if [[ -z $NOISE_PATH ]]; then
+    exit
 fi
-
 eval NOISE="$HOME/$NOISE_PATH"
-# this line basically takes screenshot, scales it way down (to blur) then blends with noise PNG. everything happens in pipes, only result is outputted
-grim - | magick png:- -scale 10% png:- | composite -blend 15 png:$NOISE png:- png:- | magick png:- -scale 1000% png:/tmp/lock.png
+
+LOCK_ARGS="-f"
+
+# Get all output names from swaymsg
+outputs=$(swaymsg -t get_outputs | jq -r '.[].name')
+
+for output in $outputs; do
+    TMP_FILE="/tmp/lock_${output}.png"
+
+    # Capture only this output with -o flag
+    grim -o "$output" - \
+        | magick png:- -scale 10% png:- \
+        | composite -blend 15 png:$NOISE png:- png:- \
+        | magick png:- -scale 1000% "$TMP_FILE"
+
+    LOCK_ARGS="$LOCK_ARGS --image ${output}:${TMP_FILE}"
+done
 
 #mpc pause
-
-swaylock -f --image /tmp/lock.png
+swaylock $LOCK_ARGS
 
