d87ba0b8321db540298121911ee19518605531c6
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2026-04-27 14:15:29 +0100

    bugfi

--- sway/scripts/screenshot-output.sh
+++ sway/scripts/screenshot-output.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 pkill slurp || hyprshot --freeze -m output \
-    --filename "$HOME/Pictures/Screenshots/$(date +%Y_%m_%d-%H%M%S).png" \
+    --filename "Pictures/Screenshots/$(date +%Y_%m_%d-%H%M%S).png" \
     --notif-timeout 5000
 
